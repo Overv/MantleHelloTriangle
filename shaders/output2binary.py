d8bec56136d95a315359345188327abe958c4e0b
Clément Guérin <libcg@protonmail.com>
2020-09-05 22:49:38 -0700

    output2binary: handle more that one word per line
    example:
    
    v_cndmask_b32  v16, s4, v0, vcc                       // 000000000118: 00200004
    v_madmk_f32   v136, s0, 0x41100000, v0                // 00000000011C: 41100000 41100000
    v_cndmask_b32  v0, s0, v0, vcc                        // 000000000124: 00000000
    v_madmk_f32   v136, s0, 0x00000079, v0                // 000000000128: 41100000 00000079
    v_cndmask_b32  v16, s5, v0, vcc                       // 000000000130: 00200005

--- shaders/output2binary.py
+++ shaders/output2binary.py
@@ -5,7 +5,8 @@ with open(sys.argv[1]) as f:
     lines = f.readlines()
 
 lines = filter(lambda l: '//' in l, lines)
-lines = map(lambda l: l.strip()[-8:], lines)
+lines = map(lambda l: l.strip().split(': ')[1].split(' '), lines)
+lines = map(lambda l: ''.join(reversed(l)), lines)
 lines = map(lambda l: re.findall('..', l)[::-1], lines)
 lines = map(lambda l: map(lambda b: chr(int(b, 16)), l), lines)
 
